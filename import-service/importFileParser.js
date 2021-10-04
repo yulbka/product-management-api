90df85f6e70992e5bf11b32aeae76d7d9700777c
romanovskayaya <yulia.romanovskaya@oxagile.com>
2021-10-04 19:23:23 +0300

    TASK-6: AWS SQS/SNS

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -5,6 +5,8 @@ const BUCKET = 'task-5-data';
 
 export async function importFileParser(event) {
     const s3 = new AWS.S3({ region: 'eu-west-1', signatureVersion: 'v4' });
+    const sqs = new AWS.SQS();
+
     event.Records.forEach(record => {    
       console.log(`record: ${record}`);    
       const s3Stream = s3.getObject({
@@ -15,6 +17,13 @@ export async function importFileParser(event) {
       s3Stream.pipe(csv())
           .on('data', (data) => {
               console.log(`importFileParser data: ${data}`);
+              const res = JSON.stringify(data);
+              sqs.sendMessage({
+                QueueUrl: process.env.SQS_URL,
+                MessageBody: res,
+            }, () => {
+                console.log(`Send message for: ${res}`);
+            });
           });
   });
 }
